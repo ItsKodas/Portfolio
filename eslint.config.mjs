1ffba00a97e5582a8bf19460fe3b2ddf6875c45b
Koda <koda@horizons.gg>
2025-12-13 03:29:45 +1000

    fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.js", "**/*.ts", "**/*.tsx"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
